dd77486aa50501c6b7c5135fa6079530f36fa0f1
RishardDukes <144169434+RishardDukes@users.noreply.github.com>
2024-05-01 21:30:17 -0400

    Update MyTrainings.py

--- MyTrainings.py
+++ MyTrainings.py
@@ -4,23 +4,6 @@ def train_model(model, train_loader, num_epochs=10, lr=0.001):
     loss_fn = tf.keras.losses.BinaryCrossentropy()
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
-<<<<<<< HEAD
-    for epoch in range(num_epochs):
-        epoch_loss = tf.keras.metrics.Mean()
-
-        for images in train_loader:
-            with tf.GradientTape() as tape:
-                outputs = model(images)
-                loss = loss_fn(images, outputs)  # Reconstruction loss
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-            epoch_loss(loss)
-
-        print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss.result():.4f}")
-
-    print("Training complete!")
-
-=======
     loss_values = []
     accuracy_values = []
 
@@ -44,4 +27,5 @@ def train_model(model, train_loader, num_epochs=10, lr=0.001):
 
     print("Training complete!")
     return loss_values, accuracy_values
->>>>>>> e63ff97 (Adding everything)
+
+
